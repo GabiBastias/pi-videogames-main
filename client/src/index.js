6ecf1f1ec33783c1b3af977205c7a79cf7523032
Federico <gabi.bastias10@gmail.com>
2023-08-29 18:56:39 -0300

    Peticiones al back deployado

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 import store from './redux/store/store';
 import axios from 'axios';
 
-axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "https://pi-videogames-main-production-95ae.up.railway.app/";
 
 ReactDOM.render(
   <Provider store={store}>
