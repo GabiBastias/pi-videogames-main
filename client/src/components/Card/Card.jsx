69b43be6336c6ebb3d96bbf2e370d1de282531cd
Federico <gabi.bastias10@gmail.com>
2023-08-29 15:00:10 -0300

    Cambios en deploy css card

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -6,7 +6,7 @@ const Card = ({ data }) =>{
         <div className={styles.divCard}>
             <NavLink className={styles.cardImg} to={`/detail/${data.id}`}>
                 <p className={styles.name}>{data.name}</p>
-                <img src={data.image} alt={data.name}/>
+                <img className={styles.img} src={data.image} alt={data.name}/>
                 <p className={styles.rating}>Rating: {data.rating}{"\u2B50"}</p>
                 <p className={styles.genres}>{data.genres.map(genre => `${genre} `)}</p>
             </NavLink>
